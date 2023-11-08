9ef62ce5a987a53d713a7f01c3ffb869d19c1836
Lillian Gensolin <LGensolin@gridprotectionalliance.org>
2023-11-08 15:00:00 -0500

    Initial Commit

---
+++ src/Gemstone.Security/UserData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gemstone.Security
+{
+    internal class UserData
+    {
+    }
+}
